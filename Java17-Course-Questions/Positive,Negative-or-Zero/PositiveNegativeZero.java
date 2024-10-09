b1da48af50a2644ae062929aac096a277effc019
Kaveesh Yoshitha <yoshithakaveesh83@gmail.com>
2024-10-09 21:13:06 +0530

    PositiveNegativeZero.java added

---
+++ Java17-Course-Questions/Positive,Negative-or-Zero/PositiveNegativeZero.java
@@ -0,0 +1,11 @@
+public class PositiveNegativeZero {
+    // write code here
+    public static void main(String[] args) {
+        checkNumber(5);
+        checkNumber(-5);
+        checkNumber(0);
+    }
+    public static void checkNumber(int number){
+       System.out.println((number>0) ? "positive":(number<0)?"negative":"zero");
+    }
+}
